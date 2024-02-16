8ca95b1d183501b041b67fad0fec375de4b153ac
Alexandra <a1excpunk@users.noreply.github.com>
2024-02-16 18:14:34 +0400

    DELETE: canvas dom manipulation code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 let burger_menu = document.querySelector(".burger_menu");
 let side_menu_close = document.querySelector(".side_menu_close");
-let side_menu = document.querySelector(".side_menu");
+let side_menu = document.querySelector(".side_menu_wrapper");
 let modal = document.querySelector(".modal");
-let bubbles = document.querySelector(".bubbles");
-let canvas1 = document.getElementById("canvas1");
 function closeSideBar() {
   side_menu.style.transform = "translateX(-100%)";
   modal.style.display = "none";
@@ -15,23 +13,3 @@ burger_menu.addEventListener("click", () => {
 });
 side_menu_close.addEventListener("click", closeSideBar);
 modal.addEventListener("click", closeSideBar);
-
-let bubbles_on = false;
-
-function turnOnBubbles() {
-  bubbles_on = true;
-  canvas1.style.zIndex = 0;
-  bubbles.textContent = "turn off bubbles";
-  bubbles.style.zIndex = 1;
-  bubbles.classList.replace("bubbles_off", "bubbles_on");
-}
-function turnOffBubbles() {
-  bubbles_on = false;
-  canvas1.style.zIndex = -1;
-  bubbles.textContent = "turn on bubbles";
-  bubbles.classList.replace("bubbles_on", "bubbles_off");
-}
-
-bubbles.addEventListener("click", () => {
-  !bubbles_on ? turnOnBubbles() : turnOffBubbles();
-});
