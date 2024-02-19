df7d5dbe5a5d534ec8c91f8538123e239bb95dc5
Alexandra <a1excpunk@users.noreply.github.com>
2024-02-19 20:33:34 +0400

    ADD: side menu and modal close functionaslity on menu item click

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
+// Side menu
 let burger_menu = document.querySelector(".burger_menu");
 let side_menu_close = document.querySelector(".side_menu_close");
+let side_menu_items = document.querySelectorAll(".side_menu_item");
 let side_menu = document.querySelector(".side_menu_wrapper");
 let modal = document.querySelector(".modal");
 function closeSideBar() {
   side_menu.style.transform = "translateX(-100%)";
   modal.style.display = "none";
 }
-
 burger_menu.addEventListener("click", () => {
   side_menu.style.transform = "translateX(0)";
   modal.style.display = "block";
 });
+side_menu_items.forEach((item) => {
+  item.addEventListener("click", closeSideBar);
+});
 side_menu_close.addEventListener("click", closeSideBar);
 modal.addEventListener("click", closeSideBar);
 
+// Scroll menu
 let test = 0;
 let test1 = 0;
 let test2 = 0;
